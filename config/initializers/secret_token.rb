1d495ca9725bbb80d90fd9ba53f12a6c5da777b9
Erez Rabih <erez@sygen.co>
2012-05-31 07:27:25 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarx::Application.config.secret_token = '3d9758af0dffcbb4eb4b4274f5f54c47279094ea289ba63622d3b769d597d5df5758ed6ac9a8924f36392cfa9ca14c9c33f4583851904dd5e5f75c3fc2dd90c9'
